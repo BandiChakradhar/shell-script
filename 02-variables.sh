8819609f53f8dc1fae9ca2ac9c9396f5592e5992
chakradhar <bandichakradhar15@gmail.com>
2025-01-27 13:13:05 +0530

    conversation added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bib/bash
 
-echo "rahim: hi robert"
-echo "robert: hi rahim"
-echo "rahim: how are you doing"
-echo "robert: i am good. how are you"
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2: hi $PERSON1"
+echo "$PERSON1: how are you doing"
+echo "$PERSON2: i am good. how are you"
