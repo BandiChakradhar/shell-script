cef380c1e1a2cd2bf935736546ce6ac631d2620b
chakradhar <bandichakradhar15@gmail.com>
2025-01-28 11:52:43 +0530

    conversation added

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -5,7 +5,27 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "error: user must have sudo access"
+    exit 1
 fi
 
-dnf install mysql -y
+dnf install mysqll -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing mysql failure"
+    exit 1
+else 
+    echo "installing mysql success"
+fi
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "installing git failure"
+    exit 1
+else 
+    echo "installing git success"
+fi
+
+
 
