209d9219e96a0513852fc23904df8cbc872ec103
chakradhar <bandichakradhar15@gmail.com>
2025-01-28 12:03:26 +0530

    conversation added

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -10,7 +10,7 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then
    dnf install mysql -y
     if [ $? -ne 0 ]
@@ -35,7 +35,7 @@ fi
 
 dnf list installed git
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then
    dnf install git -y
     if [ $? -ne 0 ]
