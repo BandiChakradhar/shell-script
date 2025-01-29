7c5e75f4008ce1e868eb0b2970d32583de938c03
chakradhar <bandichakradhar15@gmail.com>
2025-01-29 14:44:34 +0530

    delete logs

---
+++ 09-delete-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+SOURCE-DIR="/home/ec2-user/app-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failure"
+        exit 1
+    else 
+        echo "$2 success"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "error: user must have sudo access"
+        exit 1
+    fi
+}
+
+echo "script executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILE_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "files to be deleted:$FILES_TO_DELETE"
