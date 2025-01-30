14e35b586e11bdc98b7ba284fec7e8d9494dbdb6
chakradhar <bandichakradhar15@gmail.com>
2025-01-30 13:05:10 +0530

    backup logs

---
+++ 10-backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failure"
+        exit 1
+    else 
+        echo "$2 success"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "error: user must have sudo access"
+        exit 1
+    fi
+}
+
+
+USAGE(){
+    echo -e "USAGE:: sh 10-backup.sh <source_dir> <dest_dir> <days(optional)>"
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+echo "script executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
